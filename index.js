6f20996ec6fa4c6486bdc90f14b3e0974fe294a9
Saffah Khatib <skhatib1910@gmail.com>
2023-08-14 12:11:42 +0200

    changes

--- index.js
+++ index.js
@@ -11,16 +11,13 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-    // console.log( greetFunction.getGreeting());
 
     res.render('index', {
         greeting: greetFunction.getGreeting(),
         count: greetFunction.counter()
-
     });
 
 });
@@ -30,49 +27,30 @@ app.post('/greetings', function (req, res) {
     greetFunction.getNameCounter(req.body.userName);
     console.log(greetFunction.counter());
     console.log(greetFunction.getCounter(req.body.userName));
+    res.redirect('/');
+  
+});
+
+app.get('/greeted', function (req, res) {
+    res.render('greeted',{
+        names_greeted : greetFunction.objectListNames(),
+    }
+    );
     console.log(greetFunction.getGreetedNames());
-    res.redirect('/');
-    // console.log(req.body.userName);
-    // console.log(req.body.radioLanguage);
-});
-// app.post('/action', function (req, res) {
-//     //  console.log(req.body.radioLanguage);
-//     greetFunction.makeGreet(req.body.radioLanguage)
-//     res.redirect('/');
-// });
-app.post('/greeted', function (req, res) {
-
-    greetFunction.objectListNames();
-    res.redirect('/');
 
 });
-app.post('/actions', function (req, res) {
+app.get('/counter', function (req, res) {
 
-    // console.log("ACIONS.....",time());
+    res.render('counter',{
+        nameCounter : greetFunction.getGreetedNames(),
+    }
+    );
 
-    // res.render('greeted',
-        
-            greetFunction.getGreetedNames()
-
-    // );
 
 });
-
-
-// app.post('/actions', function (req, res) {
-
-// });
-//get route to display names
-// app.post('/actions/:type', function (req, res) {
-//     console.log(greetFunction.getCounter(req.body.userName));
-
-// });
 
 const PORT = process.env.PORT || 3015;
 
 app.listen(PORT, function () {
     console.log("App started at port", PORT);
 });
-// app.listen(3015, function () {
-//     console.log("App started at port");
-// });
