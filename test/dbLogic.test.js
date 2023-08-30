2e3d126b9bf2c90b2a80debaf12ed3a176e0a916
Saffah Khatib <skhatib1910@gmail.com>
2023-08-30 09:42:26 +0200

    fixed tests

--- test/dbLogic.test.js
+++ test/dbLogic.test.js
@@ -1,11 +1,7 @@
 import assert from 'assert';
-// import Greet from '../greet-factory.js';
 import namesQuery from '../service/query.js';
 import pgPromise from 'pg-promise';
 import 'dotenv/config';
-import { after } from 'mocha';
-
-
 
 const pgp = pgPromise({})
 const connectionString = process.env.DATABASE_URL;
@@ -26,19 +22,15 @@ describe('namesQuery', function () {
         it('updates and inserts a name', async () => {
             const name = 'saffah'
         
-            // Assuming query.updateAndInsertName returns a Promise indicating success or failure
             const success = await query.updateAndInsertName(name);
         
-            // Assert that the update operation was successful
             assert.isTrue(success);
         
-            // Now, you could query the database to check if the update has been applied
             const updatedRecord = await db.oneOrNone(
                 "SELECT * FROM greeting_table WHERE LOWER(names) = $1",
                 ['saffah']
             );
         
-            // Assert that the record was updated as expected
             assert.strictEqual(updatedRecord.amount_greeted, expectedNewAmount);
         });
         
@@ -58,55 +50,25 @@ describe('namesQuery', function () {
     it("should not update or insert for an invalid name", async function () {
         const invalidName = '123HI';
     
-        // Assuming query.updateAndInsertName returns a Promise indicating success or failure
         const success = await query.updateAndInsertName(invalidName);
     
-        // Assert that the update operation was not successful
         assert.strictEqual(success, false);
     
-        // Now, query the database to check if any changes were made
         const updatedRecord = await db.oneOrNone(
             "SELECT * FROM greeting_table WHERE LOWER(names) = $1",
             ['123HI']
         );
     
-        // Assert that no record exists for the invalid name
         assert.strictEqual(updatedRecord, null);
     });
 
 
     it("should clear the counter when the reset button is clicked", async function(){
-        // Insert names and get initial user counts
-        await query.updateAndInsertName('asiphe');
         await query.updateAndInsertName('saffah');
-        
-        const initialResults = await query.getGreetedNames();
-    
-        const initialUsers = 
-        {
-          asiphe: 1,
-          saffah: 1
-        };
-        
-        assert.deepEqual(initialResults, initialUsers);
-    
-        // Clear the database table
-        await query.clearDbTable();
-    
-        // Call updateAndInsertName again and expect an empty object
-        const resetResults = await query.updateAndInsertName();
-    
-        const resetUsers = {};
-        
-        assert.deepEqual(resetResults, resetUsers);
+        let initialResults = await query.counter();
+        assert.equal(initialResults, 1);
+        await query.clearDbTable()
+        let resetResults = await query.counter();
+        assert.equal(resetResults, 0);
     });
-    
-    
-    
-    
-
-    // after(function(){
-    //     debugPort.$pool.end;
-    // });
-
 });
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,8 +1,6 @@
 // import assert from 'assert';
-// // import Greet from '../greet-factory.js';
-// import namesQuery from '../service/query.js';
+//  import Greet from '../greet-factory.js';
 // import pgPromise from 'pg-promise';
-// import { beforeEach } from 'mocha';
 // import 'dotenv/config';
 
 
@@ -12,91 +10,56 @@
 // const db = pgp(connectionString);
 
 
-// // describe("Greet function", function () {
-// //     describe("Inputs", function () {
+// describe("Greet function", function () {
+//     describe("Inputs", function () {
 
-// //         it("if the name Amy is entered into the textbox and the language Xhosa is selected at the radio button return the message Molo, Amy", function () {
-// //             var greetTests = Greet();
-// //             assert.equal("", greetTests.getGreeting('amy', 'xhosa'));
-// //         });
+//         it("if the name Amy is entered into the textbox and the language Xhosa is selected at the radio button return the message Molo, Amy", function () {
+//             var greetTests = Greet();
+//             assert.equal("", greetTests.getGreeting('amy', 'xhosa'));
+//         });
 
-// //         it('if the name Bob is entered into the textbox and the language English is selected at the radio button return the message Hello, Bob', function () {
-// //             var greetTests = Greet();
-// //             assert.equal("", greetTests.getGreeting("bob", "english"));
+//         it('if the name Bob is entered into the textbox and the language English is selected at the radio button return the message Hello, Bob', function () {
+//             var greetTests = Greet();
+//             assert.equal("", greetTests.getGreeting("bob", "english"));
 
-// //         });
-// //         it('if the name Peter is entered into the textbox and the language Afrikaans is selected at the radio button return the message Hallo, Peter', function () {
-// //             var greetTests = Greet();
-// //             assert.equal("", greetTests.getGreeting("peter", "afrikaans",));
+//         });
+//         it('if the name Peter is entered into the textbox and the language Afrikaans is selected at the radio button return the message Hallo, Peter', function () {
+//             var greetTests = Greet();
+//             assert.equal("", greetTests.getGreeting("peter", "afrikaans",));
 
-// //         });
-// //     });
-
-
-
-// //     describe("Errors", function () {
-// //         it('if no name and no languge is selected return message : Please enter a name & select a language', function () {
-// //             var greetTests = Greet();
-// //             assert.equal("Please enter a name & select a language", greetTests.setErrors("", ""));
-// //         });
-// //         it("if no name is entered but only a language is selected return message : Please enter a name", function () {
-// //             var greetTests = Greet();
-// //             assert.equal("Please enter a name", greetTests.setErrors("", !""));
-// //         });
-// //         it("if no language is selected but name is entered return the message : Please select a language", function () {
-// //             var greetTests = Greet();
-// //             assert.equal("Please select a language", greetTests.setErrors(!"", ""));
-// //         });
-
-// //     });
-
-// //     describe("Return the right class for each error message", function () {
-// //         it("should return a class name of 'red' if there is an no/invalid data is entered", function () {
-// //             let greetTests = Greet();
-
-// //             assert.equal("red", greetTests.addRed())
-// //         });
-// //         it("should return a class name of 'green' if the counter has successfully reset", function () {
-// //             let greetTests = Greet();
-// //             assert.equal("green", greetTests.addGreen());
-// //         });
-
-// //     });
-// // });
-
-
-// describe('namesQuery', () => {
-
-    
-//     beforeEach(async function(){
-//         await db.none("DELETE FROM greeting_table")
-//     })
-
-
-//     it('updates and inserts a name', async () => {
-
-//         const updateNames = await query.updateAndInsertName(name);
-
-//         assert.equal(updateNames.name, 'saffah')
+//         });
 //     });
 
-//     it('retrieves greeted names', async () => {
-//         const query = namesQuery(db);
-//         const greetedNames = await query.getGreetedNames();
+
+
+//     describe("Errors", function () {
+//         it('if no name and no languge is selected return message : Please enter a name & select a language', function () {
+//             var greetTests = Greet();
+//             assert.equal("Please enter a name & select a language", greetTests.setErrors("", ""));
+//         });
+//         it("if no name is entered but only a language is selected return message : Please enter a name", function () {
+//             var greetTests = Greet();
+//             assert.equal("Please enter a name", greetTests.setErrors("", !""));
+//         });
+//         it("if no language is selected but name is entered return the message : Please select a language", function () {
+//             var greetTests = Greet();
+//             assert.equal("Please select a language", greetTests.setErrors(!"", ""));
+//         });
+
 //     });
 
-//     it('returns the total counter', async () => {
-//         const query = namesQuery(db);
-//         const totalCount = await query.counter();
+//     describe("Return the right class for each error message", function () {
+//         it("should return a class name of 'red' if there is an no/invalid data is entered", function () {
+//             let greetTests = Greet();
+
+//             assert.equal("red", greetTests.addRed())
+//         });
+//         it("should return a class name of 'green' if the counter has successfully reset", function () {
+//             let greetTests = Greet();
+//             assert.equal("green", greetTests.addGreen());
+//         });
+
 //     });
-
-//     it("should not update or insert for an invalid name", async function () {
-//         // Call the function with an invalid name
-//         await query.updateAndInsertName("123", dbStub);
-
-//         // Assert that no database methods were called
-//         assert.isTrue(dbStub.oneOrNone.notCalled);
-//         assert.isTrue(dbStub.none.notCalled);
-//     });
-
 // });
+
+
